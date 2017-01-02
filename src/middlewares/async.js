ffaef223630ddd0f20635b4515bf4bfa0a91aa0e
Chā <chavis.clark@gmail.com>
2017-01-02 16:49:47 -0500

    wire up async middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,13 @@
 export default function ({ dispatch }) {
   return next => action => {
-    console.log(action);
+    if (!action.payload || !action.payload.then) {
+      return next (action);
+    }
 
-    next(action);
+    action.payload
+      .then(function(response) {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction)
+      })
   }
 }
